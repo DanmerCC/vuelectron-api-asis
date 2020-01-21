6b04bb940064dfa1e6a9f579bc7029d94e05d564
Danmer Ccoscco <danmerccoscco@gmail.com>
2020-01-21 16:13:25 -0500

    primera ímpresion_

---
+++ src/renderer/components/Asistencias.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <modal v-if="loginVisible" @close='close()'></modal>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Modal from './Modal'
+export default {
+  name: 'asistencias',
+  components: { Modal },
+  data () {
+    return {
+      loginVisible: true
+    }
+  },
+  methods: {
+    close () {
+      this.loginVisible = false
+    }
+  },
+  mounted () {
+    axios
+      .get('http://dummy.restapiexample.com/api/v1/employees')
+      .then(response => {
+        console.log(response)
+      })
+  }
+}
+</script>
+
+<style></style>
